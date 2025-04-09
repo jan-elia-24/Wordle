20dbcfa9a5a0d3cd554694f96e9a3f3c67485a30
Jan Elia <jan.elia995@hotmail.com>
2025-04-10 00:09:44 +0200

    adding tailwind to the project

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,13 @@
 import express from "express";
+import cors from "cors";
+import path from "path";
+import fs from "fs";
 
 const app = express();
 const PORT = 5080;
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`✅ Servern körs på http://localhost:${PORT}`);
