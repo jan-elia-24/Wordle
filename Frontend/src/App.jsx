90b99f92f5a17ff7c207bc4954e627b3c70e69ae
Jan Elia <jan.elia995@hotmail.com>
2025-04-10 19:10:50 +0200

    adding wordslist to the code

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import LetterBoard from './components/letterBoard'
 
 function App() {
 
+  //const [guessedWords, setGuessedWords] = useState([])*/
   return (
-    <>
+    <LetterBoard />
      
-    </>
+  
   )
 }
 
